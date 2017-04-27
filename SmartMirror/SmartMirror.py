9b882e5b526650aaa5a9a25b2877352bad707598
Nhat Dao <daoanhnhat1995@gmail.com>
2017-04-27 15:39:24 -0500

    updated event emitter

--- SmartMirror/SmartMirror.py
+++ SmartMirror/SmartMirror.py
@@ -8,12 +8,16 @@ import os
 app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
-#r = redis.StrictRedis(host='localhost',port=6379, db=0)
+r = redis.from_url("redis://h:p06b10c763b6d7785f6c7c7f27b6152493b47abef3a0a76619cc8951b22b5f92e@ec2-34-204-242-91.compute-1.amazonaws.com:19749")
+
+@app.route("/test")
+def test():
+    return "Successs"
 
 @ask.intent('HelloWorldIntent')
 def hello_world():
     speech_text = 'Hello world'
-    # r.publish('hello','new message from me')
+    r.publish('hello','new message from me')
     return statement(speech_text).simple_card('HelloWorld', speech_text)
 
 
@@ -27,7 +31,6 @@ def help():
 def navigate(route):
     speech_text = str(route)
     r.publish('navigation',route)
-    # r.publish('hello','new message from me')
     print("NAVIGATING")
     return statement(speech_text).simple_card('Routing to',speech_text)
 
@@ -37,6 +40,7 @@ def create_profile():
     speech_text = "Let's get started. What is your name?"
     reprompt_speech_text = "May I please have your name?"
     return question(speech_text).reprompt(reprompt_speech_text)
+
 @ask.intent('MyNameIsIntent')
 def my_name_is(firstname):
     speech_text= "Hi {}, please position your face at the center of mirror".format(firstname)
@@ -46,6 +50,6 @@ def my_name_is(firstname):
 def session_ended():
     return "", 200
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    PORT = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=PORT)
